c05ea579cf2c9da585e6cb4b693f71f82869e6a9
Gionata Boccalini <gionata.boccalini@gmail.com>
2017-10-23 00:03:09 +0200

    Added first version of crossfade algorithm, very basic.

--- SmartLamp/main.cpp
+++ SmartLamp/main.cpp
@@ -48,7 +48,7 @@
 // Serial defines
 #define SERIAL_BAUD      9600   // For at mode and for data mode (CC41, HM-10 and MLT-BT05)
 
-#define SLEEP_TIMEOUT   5000L   // Timeout before sleep
+#define SLEEP_TIMEOUT  40000L   // TODO Timeout before sleep
 #define LENGTH             80   // Command buffer length
 
 #define RULES_TZ_ADD        0   // Timezone rules EEPROM start address
@@ -72,6 +72,32 @@ Timezone myTZ(CET, CEST);       // Constructor to build object with TimeChangeRu
 
 TimeChangeRule *tcr;            // Pointer to the time change rule, use to get TZ abbreviations
 
+// Color arrays
+int black[3]  = { 0, 0, 0 };
+int white[3]  = { 100, 100, 100 };
+int red[3]    = { 100, 0, 0 };
+int green[3]  = { 0, 100, 0 };
+int blue[3]   = { 0, 0, 100 };
+int yellow[3] = { 40, 95, 0 };
+int dimWhite[3] = { 30, 30, 30 };
+// etc.
+
+// Set initial color
+int redVal = dimWhite[0];
+int grnVal = dimWhite[1];
+int bluVal = dimWhite[2];
+
+int wait = 5;      // 10ms internal crossFade delay; increase for slower fades
+int hold = 0;       // Optional hold when a color is complete, before the next crossFade
+int DEBUG = 1;      // DEBUG counter; if set to 1, will write values back via serial
+int loopCount = 60; // How often should DEBUG report?
+int repeat = 3;     // How many times should we loop before stopping? (0 for no stop)
+int j = 0;          // Loop counter for repeat
+
+// Initialize color variables
+int prevR = redVal;
+int prevG = grnVal;
+int prevB = bluVal;
 
 // Put the micro to sleep
 void system_sleep() {
@@ -143,8 +169,112 @@ ISR(PCINT0_vect) {
   // uninitialized interrupt handler.
 }
 
-time_t timeProvider() {
-  return RTC.get();
+/* BELOW THIS LINE IS THE MATH -- YOU SHOULDN'T NEED TO CHANGE THIS FOR THE BASICS
+*
+* The program works like this:
+* Imagine a crossfade that moves the red LED from 0-10,
+*   the green from 0-5, and the blue from 10 to 7, in
+*   ten steps.
+*   We'd want to count the 10 steps and increase or
+*   decrease color values in evenly stepped increments.
+*   Imagine a + indicates raising a value by 1, and a -
+*   equals lowering it. Our 10 step fade would look like:
+*
+*   1 2 3 4 5 6 7 8 9 10
+* R + + + + + + + + + +
+* G   +   +   +   +   +
+* B     -     -     -
+*
+* The red rises from 0 to 10 in ten steps, the green from
+* 0-5 in 5 steps, and the blue falls from 10 to 7 in three steps.
+*
+* In the real program, the color percentages are converted to
+* 0-255 values, and there are 1020 steps (255*4).
+*
+* To figure out how big a step there should be between one up- or
+* down-tick of one of the LED values, we call calculateStep(),
+* which calculates the absolute gap between the start and end values,
+* and then divides that gap by 1020 to determine the size of the step
+* between adjustments in the value.
+*/
+int calculateStep(int prevValue, int endValue) {
+  int step = endValue - prevValue; // What's the overall gap?
+  if (step) {                      // If its non-zero,
+    step = 1020/step;              //   divide by 1020
+  }
+  return step;
+}
+
+/* The next function is calculateVal. When the loop value, i,
+*  reaches the step size appropriate for one of the
+*  colors, it increases or decreases the value of that color by 1.
+*  (R, G, and B are each calculated separately.)
+*/
+int calculateVal(int step, int val, int i) {
+
+  if ((step) && i % step == 0) { // If step is non-zero and its time to change a value,
+    if (step > 0) {              //   increment the value if step is positive...
+      val += 1;
+    }
+    else if (step < 0) {         //   ...or decrement it if step is negative
+      val -= 1;
+    }
+  }
+  // Defensive driving: make sure val stays in the range 0-255
+  if (val > 255) {
+    val = 255;
+  }
+  else if (val < 0) {
+    val = 0;
+  }
+  return val;
+}
+
+/* crossFade() converts the percentage colors to a
+*  0-255 range, then loops 1020 times, checking to see if
+*  the value needs to be updated each time, then writing
+*  the color values to the correct pins.
+*/
+void crossFade(int color[3]) {
+  // Convert to 0-255
+  int R = (color[0] * 255) / 100;
+  int G = (color[1] * 255) / 100;
+  int B = (color[2] * 255) / 100;
+
+  int stepR = calculateStep(prevR, R);
+  int stepG = calculateStep(prevG, G);
+  int stepB = calculateStep(prevB, B);
+
+  for (int i = 0; i <= 1020; i++) {
+    redVal = calculateVal(stepR, redVal, i);
+    grnVal = calculateVal(stepG, grnVal, i);
+    bluVal = calculateVal(stepB, bluVal, i);
+
+    analogWrite(LED_RED, redVal);   // Write current values to LED pins
+    analogWrite(LED_GR, grnVal);
+    analogWrite(LED_BLUE, bluVal);
+
+    delay(wait); // Pause for 'wait' milliseconds before resuming the loop
+
+    if (DEBUG) { // If we want serial output, print it at the
+      if (i == 0 or i % loopCount == 0) { // beginning, and every loopCount times
+        Serial.print("Loop/RGB: #");
+        Serial.print(i);
+        Serial.print(" | ");
+        Serial.print(redVal);
+        Serial.print(" / ");
+        Serial.print(grnVal);
+        Serial.print(" / ");
+        Serial.println(bluVal);
+      }
+      DEBUG += 1;
+    }
+  }
+  // Update current values for next loop
+  prevR = redVal;
+  prevG = grnVal;
+  prevB = bluVal;
+  delay(hold); // Pause for optional 'wait' milliseconds before resuming the loop
 }
 
 void setup() {
@@ -191,7 +321,7 @@ void setup() {
   Serial.print(F("Local: "));
   digitalClockDisplay(local, tcr->abbrev);
 
-
+  // Power settings
   ADCSRA  = 0;                      // Disable ADC to save power
   MCUCR  |= _BV(BODS);              // BOD disabled
 
@@ -248,45 +378,35 @@ void loop() {
     data = false;
 
     if (strcmp(buffer, "ON") == 0) {
-      digitalWrite(LED_BLUE, HIGH);
-
       RTC.setAlarm(ALM1_MATCH_MINUTES, 00, 10, 23, 0);
       RTC.alarmInterrupt(ALARM_1, true);
 
-//      for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) {
-//          analogWrite(LED_BLUE, fadeValue);
-//          delay(100);
+      crossFade(red);
+      crossFade(green);
+      crossFade(blue);
+      crossFade(yellow);
+
+//      if (repeat) { // Do we loop a finite number of times?
+//        j += 1;
+//
+//      if (j >= repeat) { // Are we there yet?
+//          exit(j);       // If so, stop.
 //        }
-//      delay(1000);
-//      digitalWrite(LED_BLUE, LOW);
-//
-//      for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) {
-//        analogWrite(LED_RED, fadeValue);
-//        delay(100);
 //      }
-//      delay(1000);
-//      digitalWrite(LED_RED, LOW);
-//
-//      for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) {
-//        analogWrite(LED_GR, fadeValue);
-//        delay(100);
-//      }
-//      delay(1000);
-//      digitalWrite(LED_GR, LOW);
     }
 
     if (strcmp(buffer, "OFF") == 0) {
-      digitalWrite(LED_BLUE, LOW);
-      digitalWrite(LED_RED, LOW);
-      digitalWrite(LED_GR, LOW);
+      analogWrite(LED_BLUE, 0);
+      analogWrite(LED_RED, 0);
+      analogWrite(LED_GR, 0);
     }
 
-    time_t utc = RTC.get();
-    Serial.print(F("UTC: "));
-    digitalClockDisplay(utc, "UTC");
-
-    time_t local = myTZ.toLocal(utc, &tcr);
-    Serial.print(F("Local: "));
-    digitalClockDisplay(local, tcr->abbrev);
+//    time_t utc = RTC.get();
+//    Serial.print(F("UTC: "));
+//    digitalClockDisplay(utc, "UTC");
+//
+//    time_t local = myTZ.toLocal(utc, &tcr);
+//    Serial.print(F("Local: "));
+//    digitalClockDisplay(local, tcr->abbrev);
   }
 }
