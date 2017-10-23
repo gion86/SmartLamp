b80280da1e20f471dca46e6da1f9502db8bf22ba
Gionata Boccalini <gionata.boccalini@gmail.com>
2017-10-24 00:19:16 +0200

    First version of fade algorithm with cos() function. Added state machine to loop.

--- SmartLamp/main.cpp
+++ SmartLamp/main.cpp
@@ -17,7 +17,7 @@
 
 /*
  * Built for Attiny84 8Mhz, using AVR USBasp programmer.
- * VERSION 0.2
+ * VERSION 0.3
  */
 
 #include <avr/io.h>
@@ -48,56 +48,42 @@
 // Serial defines
 #define SERIAL_BAUD      9600   // For at mode and for data mode (CC41, HM-10 and MLT-BT05)
 
-#define SLEEP_TIMEOUT  40000L   // TODO Timeout before sleep
-#define LENGTH             80   // Command buffer length
+#define SLEEP_TIMEOUT  10000L   // TODO Timeout before sleep
+
+#define LENGTH             40   // Command buffer length
 
 #define RULES_TZ_ADD        0   // Timezone rules EEPROM start address
 
+#define PERIOD          30000   // LED crossfade total duration
+#define MAX_ANALOG_V       50   // Analog output max value for RGB
+
+#define STEP_START         0
+#define STEP_READ_CMD      10
+#define STEP_FADE          15
 
 // Global constants
-//CET Time Zone (Rome, Berlin) -> UTC/GMT + 1
-const TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};     // Central European Summer Time (DST)
-const TimeChangeRule CET  = {"CET ", Last, Sun, Oct, 3, 60};      // Central European Standard Time
+// CET Time Zone (Rome, Berlin) -> UTC/GMT + 1
+// These rules are written in the EEPROM, at address RULES_TZ_ADD. The EEPROM by default gets erased by the
+// erase procedure on upload, which clears FLASH, EEPROM and lock bits. To preserve the EEPROM memory fuse
+// EESAVE has to be set. In this case EEPROM will not be erased during erase procedure.
+// But the fuse has to be cleared if you want to clear and/or reflash EEPROM.
+//const TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};     // Central European Summer Time (DST)
+//const TimeChangeRule CET  = {"CET ", Last, Sun, Oct, 3, 60};      // Central European Standard Time
 
 // Global variables
 USIWire bus;                    // USIWire instance (I2C bus)
 DS3232RTC RTC(bus);             // DS3232 RTC instance (I2C bus)
 
-boolean data = false;
+bool data = false;
 unsigned long prevMillis = 0;   // Millis counter to sleep
 bool rtcInitOk = false;         // Communication OK with RTC
 
-//Timezone myTZ(RULES_TZ_ADD);    // TODO Constructor to read rules stored at EEPROM address RULES_TZ_ADD
-Timezone myTZ(CET, CEST);       // Constructor to build object with TimeChangeRule
+Timezone myTZ(RULES_TZ_ADD);    // Constructor to read rules stored at EEPROM address RULES_TZ_ADD
+//Timezone myTZ(CET, CEST);     // Constructor to build object with TimeChangeRule
 
 TimeChangeRule *tcr;            // Pointer to the time change rule, use to get TZ abbreviations
 
-// Color arrays
-int black[3]  = { 0, 0, 0 };
-int white[3]  = { 100, 100, 100 };
-int red[3]    = { 100, 0, 0 };
-int green[3]  = { 0, 100, 0 };
-int blue[3]   = { 0, 0, 100 };
-int yellow[3] = { 40, 95, 0 };
-int dimWhite[3] = { 30, 30, 30 };
-// etc.
-
-// Set initial color
-int redVal = dimWhite[0];
-int grnVal = dimWhite[1];
-int bluVal = dimWhite[2];
-
-int wait = 5;      // 10ms internal crossFade delay; increase for slower fades
-int hold = 0;       // Optional hold when a color is complete, before the next crossFade
-int DEBUG = 1;      // DEBUG counter; if set to 1, will write values back via serial
-int loopCount = 60; // How often should DEBUG report?
-int repeat = 3;     // How many times should we loop before stopping? (0 for no stop)
-int j = 0;          // Loop counter for repeat
-
-// Initialize color variables
-int prevR = redVal;
-int prevG = grnVal;
-int prevB = bluVal;
+uint8_t step = STEP_START;
 
 // Put the micro to sleep
 void system_sleep() {
@@ -169,116 +155,12 @@ ISR(PCINT0_vect) {
   // uninitialized interrupt handler.
 }
 
-/* BELOW THIS LINE IS THE MATH -- YOU SHOULDN'T NEED TO CHANGE THIS FOR THE BASICS
-*
-* The program works like this:
-* Imagine a crossfade that moves the red LED from 0-10,
-*   the green from 0-5, and the blue from 10 to 7, in
-*   ten steps.
-*   We'd want to count the 10 steps and increase or
-*   decrease color values in evenly stepped increments.
-*   Imagine a + indicates raising a value by 1, and a -
-*   equals lowering it. Our 10 step fade would look like:
-*
-*   1 2 3 4 5 6 7 8 9 10
-* R + + + + + + + + + +
-* G   +   +   +   +   +
-* B     -     -     -
-*
-* The red rises from 0 to 10 in ten steps, the green from
-* 0-5 in 5 steps, and the blue falls from 10 to 7 in three steps.
-*
-* In the real program, the color percentages are converted to
-* 0-255 values, and there are 1020 steps (255*4).
-*
-* To figure out how big a step there should be between one up- or
-* down-tick of one of the LED values, we call calculateStep(),
-* which calculates the absolute gap between the start and end values,
-* and then divides that gap by 1020 to determine the size of the step
-* between adjustments in the value.
-*/
-int calculateStep(int prevValue, int endValue) {
-  int step = endValue - prevValue; // What's the overall gap?
-  if (step) {                      // If its non-zero,
-    step = 1020/step;              //   divide by 1020
-  }
-  return step;
-}
-
-/* The next function is calculateVal. When the loop value, i,
-*  reaches the step size appropriate for one of the
-*  colors, it increases or decreases the value of that color by 1.
-*  (R, G, and B are each calculated separately.)
-*/
-int calculateVal(int step, int val, int i) {
-
-  if ((step) && i % step == 0) { // If step is non-zero and its time to change a value,
-    if (step > 0) {              //   increment the value if step is positive...
-      val += 1;
-    }
-    else if (step < 0) {         //   ...or decrement it if step is negative
-      val -= 1;
-    }
-  }
-  // Defensive driving: make sure val stays in the range 0-255
-  if (val > 255) {
-    val = 255;
-  }
-  else if (val < 0) {
-    val = 0;
-  }
-  return val;
-}
-
-/* crossFade() converts the percentage colors to a
-*  0-255 range, then loops 1020 times, checking to see if
-*  the value needs to be updated each time, then writing
-*  the color values to the correct pins.
-*/
-void crossFade(int color[3]) {
-  // Convert to 0-255
-  int R = (color[0] * 255) / 100;
-  int G = (color[1] * 255) / 100;
-  int B = (color[2] * 255) / 100;
-
-  int stepR = calculateStep(prevR, R);
-  int stepG = calculateStep(prevG, G);
-  int stepB = calculateStep(prevB, B);
-
-  for (int i = 0; i <= 1020; i++) {
-    redVal = calculateVal(stepR, redVal, i);
-    grnVal = calculateVal(stepG, grnVal, i);
-    bluVal = calculateVal(stepB, bluVal, i);
-
-    analogWrite(LED_RED, redVal);   // Write current values to LED pins
-    analogWrite(LED_GR, grnVal);
-    analogWrite(LED_BLUE, bluVal);
-
-    delay(wait); // Pause for 'wait' milliseconds before resuming the loop
-
-    if (DEBUG) { // If we want serial output, print it at the
-      if (i == 0 or i % loopCount == 0) { // beginning, and every loopCount times
-        Serial.print("Loop/RGB: #");
-        Serial.print(i);
-        Serial.print(" | ");
-        Serial.print(redVal);
-        Serial.print(" / ");
-        Serial.print(grnVal);
-        Serial.print(" / ");
-        Serial.println(bluVal);
-      }
-      DEBUG += 1;
-    }
-  }
-  // Update current values for next loop
-  prevR = redVal;
-  prevG = grnVal;
-  prevB = bluVal;
-  delay(hold); // Pause for optional 'wait' milliseconds before resuming the loop
-}
+// ========================================================
+// |                        SETUP                         |
+// ========================================================
 
 void setup() {
-  byte retcode;
+  uint8_t retcode;
 
   OSCCAL = 0x86;                    // Calibrated OSSCAL value with TinyTuner
 
@@ -293,25 +175,15 @@ void setup() {
   pinMode(BLU_RESET, OUTPUT);
   pinMode(RTC_INT_SQW, INPUT);
 
-  Serial.println(F("\nSTART"));
-  Serial.print(F("OSCCAL = 0x"));
-  Serial.println(OSCCAL, HEX);
-
   time_t t;
   t = tmConvert_t(2017, 10, 25, 11, 00, 00);
 
   if ((retcode = RTC.set(t)) == 0)
     rtcInitOk = true;
   else {
-    Serial.print(F("RTC Set error: "));
+    Serial.print(F("RTC err: "));
     Serial.println(retcode);
   }
-
-  Serial.print(F("T: "));
-  Serial.println(t);
-  digitalClockDisplay(t, "UTC");
-
-  Serial.println();
 
   time_t utc = RTC.get();
   Serial.print(F("UTC: "));
@@ -330,83 +202,109 @@ void setup() {
   GIMSK  |= _BV(PCIE0);             // Enable PCINT interrupt on portA
 }
 
+// ========================================================
+// |                        LOOP                          |
+// ========================================================
+
 void loop() {
   size_t count = 0;
   char buffer[LENGTH];
 
-  // FIXME Wake from sleep with new CORE can't read first serial bytes....
+  uint8_t value;
+  double x = 0;
 
-  if (millis() - prevMillis >= SLEEP_TIMEOUT) {
-    prevMillis = millis();
-    Serial.println(F("Sleeping..."));
-    system_sleep();
-    Serial.println(F("Waking up..."));
-    digitalClockDisplay(RTC.get(), "UTC");
+  switch (step) {
+    case STEP_START:
 
-    // Necessary to reset the alarm flag on RTC!
-    if (RTC.alarm(ALARM_1)) {
-      Serial.println(F("From alarm..."));
-    }
-  }
+      // FIXME Wake from sleep with new CORE can't read first serial bytes....
+      if (millis() - prevMillis >= SLEEP_TIMEOUT) {
+        prevMillis = millis();
+        Serial.println(F("Sleeping..."));
+        system_sleep();
+        Serial.println(F("Waking up..."));
+        digitalClockDisplay(RTC.get(), "UTC");
 
-  while (Serial.available() && count < LENGTH - 1) {
-    delay(2);
-    char c = (char) Serial.read();
+        // Necessary to reset the alarm flag on RTC!
+        if (RTC.alarm(ALARM_1)) {
+          Serial.println(F("From alarm..."));
+        }
 
-    prevMillis = millis();      // Update prevMillis to reset sleep timeout
-
-    if (c == '\r' || c == '\n') {
-      if (c == '\n') {
-        data = true;
-        Serial.flush();
-        break;
+        step = STEP_READ_CMD;
       }
-      continue;
-    }
 
-    buffer[count] = c;
-    count++;
-  }
+      break;
 
-  if (data) {
-    buffer[count] = '\0';
-    //Serial.print("COUNT = ");
-    //Serial.println(count);
-    Serial.println(buffer);
+    case STEP_READ_CMD:
 
-    count = 0;
-    data = false;
+      while (Serial.available() && count < LENGTH - 1) {
+        delay(2);
+        char c = (char) Serial.read();
 
-    if (strcmp(buffer, "ON") == 0) {
-      RTC.setAlarm(ALM1_MATCH_MINUTES, 00, 10, 23, 0);
-      RTC.alarmInterrupt(ALARM_1, true);
+        prevMillis = millis();      // Update prevMillis to reset sleep timeout
 
-      crossFade(red);
-      crossFade(green);
-      crossFade(blue);
-      crossFade(yellow);
+        if (c == '\r' || c == '\n') {
+          if (c == '\n') {
+            data = true;
+            Serial.flush();
+            break;
+          }
+          continue;
+        }
 
-//      if (repeat) { // Do we loop a finite number of times?
-//        j += 1;
-//
-//      if (j >= repeat) { // Are we there yet?
-//          exit(j);       // If so, stop.
-//        }
-//      }
-    }
+        buffer[count] = c;
+        count++;
+      }
 
-    if (strcmp(buffer, "OFF") == 0) {
-      analogWrite(LED_BLUE, 0);
-      analogWrite(LED_RED, 0);
-      analogWrite(LED_GR, 0);
-    }
+      if (data) {
+        buffer[count] = '\0';
+        //Serial.print("COUNT = ");
+        //Serial.println(count);
+        Serial.println(buffer);
 
-//    time_t utc = RTC.get();
-//    Serial.print(F("UTC: "));
-//    digitalClockDisplay(utc, "UTC");
-//
-//    time_t local = myTZ.toLocal(utc, &tcr);
-//    Serial.print(F("Local: "));
-//    digitalClockDisplay(local, tcr->abbrev);
+        count = 0;
+        data = false;
+
+        if (strcmp(buffer, "ON") == 0) {
+          RTC.setAlarm(ALM1_MATCH_MINUTES, 00, 10, 23, 0);
+          RTC.alarmInterrupt(ALARM_1, true);
+
+          step = STEP_FADE;
+        }
+        else if (strcmp(buffer, "OFF") == 0) {
+          analogWrite(LED_BLUE, 0);
+          analogWrite(LED_RED, 0);
+          analogWrite(LED_GR, 0);
+        }
+
+        //    time_t utc = RTC.get();
+        //    Serial.print(F("UTC: "));
+        //    digitalClockDisplay(utc, "UTC");
+        //
+        //    time_t local = myTZ.toLocal(utc, &tcr);
+        //    Serial.print(F("Local: "));
+        //    digitalClockDisplay(local, tcr->abbrev);
+      }
+      break;
+
+    case STEP_FADE:
+
+      x = (millis() % PERIOD) / (double) PERIOD;
+      value = MAX_ANALOG_V * sin(PI * x);
+
+      //  Serial.print(F("t: "));
+      //  Serial.print(millis());
+      Serial.print(F(", x: "));
+      Serial.print(x);
+      Serial.print(F(", c: "));
+      Serial.println(value);
+
+      analogWrite(LED_BLUE, value);
+      analogWrite(LED_RED, value);
+      analogWrite(LED_GR, value);
+
+      break;
+
+    default:
+      break;
   }
 }
