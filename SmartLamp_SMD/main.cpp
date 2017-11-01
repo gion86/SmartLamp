878caa80fb7ddca763ce23180b555a7ddd56cce4
Gionata Boccalini <gionata.boccalini@gmail.com>
2017-11-01 17:23:07 +0100

    First build for ATTMega328p at 8MHz, with RTC, LED and state machine tested. Using optiboot bootloader.

--- SmartLamp_SMD/main.cpp
+++ SmartLamp_SMD/main.cpp
@@ -16,13 +16,14 @@
  */
 
 /*
- * Built for ATMega328P 20Mhz, using AVR USBasp programmer.
+ * Built for ATMega328P 8Mhz, using AVR USBasp programmer.
  * VERSION 0.1
  */
 
 #include <avr/io.h>
 #include <avr/sleep.h>
 #include <avr/pgmspace.h>
+#include <avr/interrupt.h>
 #include <time.h>
 
 #include <Arduino.h>
@@ -41,10 +42,6 @@
 
 #define RTC_INT_SQW         4   // INT/SQW pin from RTC
 
-// I2C defines
-#define SDA               AI4   // SDA pin
-#define SCL               AI5   // SCL pin
-
 // Serial defines
 #define SERIAL_BAUD      9600   // For at mode and for data mode (CC41, HM-10 and MLT-BT05)
 
@@ -53,10 +50,10 @@
 
 #define RULES_TZ_ADD        0   // Timezone rules EEPROM start address
 
-#define PERIOD           5000   // LED crossfade total duration
+#define PERIOD          5000L   // LED crossfade total duration
 #define MAX_ANALOG_V      255   // Analog output max value for RGB
 
-#define STEP_START         0
+#define STEP_SLEEP         0
 #define STEP_READ_CMD      10
 #define STEP_FADE          15
 
@@ -66,19 +63,20 @@
 // erase procedure on upload, which clears FLASH, EEPROM and lock bits. To preserve the EEPROM memory fuse
 // EESAVE has to be set. In this case EEPROM will not be erased during erase procedure.
 // But the fuse has to be cleared if you want to clear and/or reflash EEPROM.
-//const TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};     // Central European Summer Time (DST)
-//const TimeChangeRule CET  = {"CET ", Last, Sun, Oct, 3, 60};      // Central European Standard Time
+const TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};     // Central European Summer Time (DST)
+const TimeChangeRule CET  = {"CET ", Last, Sun, Oct, 3, 60};      // Central European Standard Time
 
 // Global variables
 TwoWire bus;                    // USIWire instance (I2C bus)
 DS3232RTC RTC(bus);             // DS3232 RTC instance (I2C bus)
 
+size_t count = 0;
 bool data = false;
 unsigned long prevMillis = 0;   // Millis counter to sleep
 bool rtcInitOk = false;         // Communication OK with RTC
 
-Timezone myTZ(RULES_TZ_ADD);    // Constructor to read rules stored at EEPROM address RULES_TZ_ADD
-//Timezone myTZ(CET, CEST);     // Constructor to build object with TimeChangeRule
+//Timezone myTZ(RULES_TZ_ADD);    // Constructor to read rules stored at EEPROM address RULES_TZ_ADD
+Timezone myTZ(CET, CEST);     // Constructor to build object with TimeChangeRule
 
 TimeChangeRule *tcr;            // Pointer to the time change rule, use to get TZ abbreviations
 
@@ -148,7 +146,7 @@ void digitalClockDisplay(time_t time, const char *tz = NULL) {
 }
 
 // PCINT Interrupt Service Routine (unused)
-ISR(PCINT0_vect) {
+ISR(PCINT2_vect) {
   // Don't do anything here but we must include this
   // block of code otherwise the interrupt calls an
   // uninitialized interrupt handler.
@@ -161,10 +159,8 @@ ISR(PCINT0_vect) {
 void setup() {
   uint8_t retcode;
 
-  //OSCCAL = 0x86;                    // Calibrated OSSCAL value with TinyTuner
-
   Serial.begin(SERIAL_BAUD);
-  //bus.begin();
+  bus.begin();
 
   // Pinmode set
   pinMode(LED_BLUE, OUTPUT);
@@ -174,8 +170,8 @@ void setup() {
   pinMode(BLU_RESET, OUTPUT);
   pinMode(RTC_INT_SQW, INPUT);
 
-  /*time_t t;
-  t = tmConvert_t(2017, 10, 25, 11, 00, 00);
+  time_t t;
+  t = tmConvert_t(2017, 11, 01, 11, 00, 00);
 
   if ((retcode = RTC.set(t)) == 0)
     rtcInitOk = true;
@@ -191,73 +187,57 @@ void setup() {
   time_t local = myTZ.toLocal(utc, &tcr);
   Serial.print(F("Local: "));
   digitalClockDisplay(local, tcr->abbrev);
-*/
+
   // Power settings
 
-  //ADCSRA  = 0;                      // Disable ADC to save power
-  //MCUCR  |= _BV(BODS);              // BOD disabled
-  //
-  //PCMSK0 |= _BV(PCINT0);            // Pin change mask: listen to portA bit 0 (D10) (RTC_INT_SQW)
-  //PCMSK0 |= _BV(PCINT2);            // Pin change mask: listen to portA bit 2  (D8) (Serial RX - AIN1)
-  //GIMSK  |= _BV(PCIE0);             // Enable PCINT interrupt on portA
+  ADCSRA  = 0;                      // Disable ADC to save power
+  MCUCR  |= _BV(BODS);              // BOD disabled
+
+  PCMSK2 |= _BV(PCINT20);           // Pin change mask: listen to portD bit 4 (D4) (RTC_INT_SQW)
+  PCMSK2 |= _BV(PCINT16);           // TODO Pin change mask: listen to portD bit 0 (D0) (Serial RX)
+  PCICR  |= _BV(PCIE2);             // Enable PCINT interrupt on portD
+
+  prevMillis = millis();
 }
 
 // ========================================================
 // |                        LOOP                          |
 // ========================================================
 
-size_t count = 0;
-
 void loop() {
   char buffer[LENGTH];
-
-  uint8_t value;
+  uint8_t led_value;
   double x = 0;
 
   switch (step) {
-    case STEP_START:
+    case STEP_SLEEP:
 
-      // FIXME Wake from sleep with new CORE can't read first serial bytes....
-      if (millis() - prevMillis >= SLEEP_TIMEOUT) {
-        prevMillis = millis();
-        Serial.println(F("Sleeping..."));
-        system_sleep();
-        Serial.println(F("Waking up..."));
-        //digitalClockDisplay(RTC.get(), "UTC");
+      Serial.println(F("Sleeping..."));
+      delay(50);
+      system_sleep();
+      Serial.println(F("Waking up..."));
+      digitalClockDisplay(RTC.get(), "UTC");
 
-        // Necessary to reset the alarm flag on RTC!
-        //if (RTC.alarm(ALARM_1)) {
-        //  Serial.println(F("From alarm..."));
-        //}
-
-        step = STEP_READ_CMD;
+      // Necessary to reset the alarm flag on RTC!
+      if (RTC.alarm(ALARM_1)) {
+        Serial.println(F("From alarm..."));
       }
+
+      prevMillis = millis();
+      step = STEP_READ_CMD;
 
       break;
 
     case STEP_READ_CMD:
 
-//      while (Serial.available()){
-//        Serial.write(Serial.read());
-//      }
-
       while (Serial.available() && count < LENGTH - 1) {
-        //delay(2);
         char c = (char) Serial.read();
-
-        //Serial.write(c);
-
-        //Serial.print(" c:");
-        //Serial.print(count);
 
         prevMillis = millis();      // Update prevMillis to reset sleep timeout
 
         if (c == '\r' || c == '\n') {
           if (c == '\n') {
             data = true;
-            //Serial.flush();
-            //Serial.print("data ok: ");
-            //Serial.println(count);
             break;
           }
           continue;
@@ -265,66 +245,75 @@ void loop() {
 
         buffer[count] = c;
         count++;
-        //Serial.print("c++:");
-        //Serial.println(count);
       }
 
       if (data) {
         buffer[count] = '\0';
-        Serial.print("COUNT = ");
-        Serial.println(count);
+        //Serial.print("COUNT = ");
+        //Serial.println(count);
         Serial.println(buffer);
 
-//        if (buffer[0] == 'S' && buffer[1] == 'T') {
-//          char d[5];
-//
-//          strlcpy(d, buffer + 2, 4);
-//
-//          d[5] = '\0';
-//          Serial.println(d);
-//
-//        }
+        /*
+        if (buffer[0] == 'S' && buffer[1] == 'T') {
+          char d[5];
 
+          strlcpy(d, buffer + 2, 4);
+
+          d[5] = '\0';
+          Serial.println(d);
+        }
+        */
 
         if (strcmp(buffer, "ON") == 0) {
-        //  RTC.setAlarm(ALM1_MATCH_MINUTES, 00, 10, 23, 0);
-        //  RTC.alarmInterrupt(ALARM_1, true);
-        //
+          RTC.setAlarm(ALM1_MATCH_MINUTES, 00, 01, 11, 0);
+          RTC.alarmInterrupt(ALARM_1, true);
           step = STEP_FADE;
+          //step = STEP_SLEEP;
+        } else if (strcmp(buffer, "OFF") == 0) {
+          analogWrite(LED_BLUE, 0);
+          analogWrite(LED_RED, 0);
+          analogWrite(LED_GR, 0);
+          step = STEP_SLEEP;
         }
-        //else if (strcmp(buffer, "OFF") == 0) {
-        //  analogWrite(LED_BLUE, 0);
-        //  analogWrite(LED_RED, 0);
-        //  analogWrite(LED_GR, 0);
-        //}
 
-        //    time_t utc = RTC.get();
-        //    Serial.print(F("UTC: "));
-        //    digitalClockDisplay(utc, "UTC");
-        //
-        //    time_t local = myTZ.toLocal(utc, &tcr);
-        //    Serial.print(F("Local: "));
-        //    digitalClockDisplay(local, tcr->abbrev);
+        time_t utc = RTC.get();
+        Serial.print(F("UTC: "));
+        digitalClockDisplay(utc, "UTC");
+
+        time_t local = myTZ.toLocal(utc, &tcr);
+        Serial.print(F("Local: "));
+        digitalClockDisplay(local, tcr->abbrev);
         count = 0;
         data = false;
+        delay(50);
       }
+
+      if (millis() - prevMillis >= SLEEP_TIMEOUT) {
+        step = STEP_SLEEP;
+      }
+
       break;
 
     case STEP_FADE:
 
       x = (millis() % PERIOD) / (double) PERIOD;
-      value = MAX_ANALOG_V * sin(PI * x);
+      led_value = MAX_ANALOG_V * sin(PI * x);
 
-      //  Serial.print(F("t: "));
-      //  Serial.print(millis());
       Serial.print(F(", x: "));
       Serial.print(x);
       Serial.print(F(", c: "));
-      Serial.println(value);
+      Serial.println(led_value);
 
-      analogWrite(LED_BLUE, value);
-      analogWrite(LED_RED, value);
-      analogWrite(LED_GR, value);
+      analogWrite(LED_BLUE, led_value);
+      analogWrite(LED_RED, led_value);
+      analogWrite(LED_GR, led_value);
+
+      if (x >= 0.99) {
+        analogWrite(LED_BLUE, 0);
+        analogWrite(LED_RED, 0);
+        analogWrite(LED_GR, 0);
+        step = STEP_READ_CMD;
+      }
 
       break;
 
