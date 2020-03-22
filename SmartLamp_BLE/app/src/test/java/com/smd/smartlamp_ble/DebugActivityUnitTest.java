0252ecdea88d585c08844f35f42a94e21ac8f934
Gionata Boccalini <gionata.boccalini@gmail.com>
2020-03-22 11:38:04 +0100

    Updated gradle plugin and app build configuration. Added debug activity DebugActivityUnitTest.java.

---
+++ SmartLamp_BLE/app/src/test/java/com/smd/smartlamp_ble/DebugActivityUnitTest.java
@@ -0,0 +1,12 @@
+package com.smd.smartlamp_ble;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DebugActivityUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ SmartLamp_BLE/app/src/test/java/com/smd/smartlamp_ble/device/DeviceDebugActivityTest.java
@@ -0,0 +1,12 @@
+package com.smd.smartlamp_ble.device;
+
+import org.junit.Test;
+
+public class DeviceDebugActivityTest {
+
+    @Test
+    public void parseData_BLE() {
+        DeviceDebugActivity activity = new DeviceDebugActivity();
+        //activity.parseData("row1\nrow2"); // Needed annotation @VisibleForTesting
+    }
+}
